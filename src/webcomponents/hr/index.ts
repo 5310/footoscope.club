f07b73f49ff12fb9209a906a9b0512cfa173842d
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2020-12-31 18:05:24 +0530

    Initial HR component

---
+++ src/webcomponents/hr/index.ts
@@ -0,0 +1,24 @@
+import { customElement, property, LitElement, html, css } from 'lit-element'
+
+@customElement('scope-hr')
+export class ScopeHR extends LitElement {
+  static get styles() {
+    return css`
+      :root {
+        display: none;
+      }
+    `
+  }
+  render() {
+    return html`
+      <link rel="stylesheet" href="/webcomponents/hr/index.css" />
+      <main>
+        <svg width="2048" height="16">
+          <g>
+            <path style="fill:none;" d="M 0,8 H 2048" />
+          </g>
+        </svg>
+      </main>
+    `
+  }
+}
